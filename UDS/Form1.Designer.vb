03c432b39671092276e6669c50ca4c2651d54dd1
MC-AGANG <54347161+MC-AGANG@users.noreply.github.com>
2021-01-16 21:25:12 +0800

    Add files via upload

---
+++ UDS/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(42, 111)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(398, 124)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "事件"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 12)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "拷贝到路径："
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(123, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(249, 21)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "D:\AGANG\UDC\"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(378, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 21)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "浏览..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FolderBrowserDialog1
+        '
+        Me.FolderBrowserDialog1.Description = "选择目标文件夹"
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "AGANG_UDS"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(210, 248)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(81, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "隐藏"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(339, 248)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(86, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "退出"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(65, 249)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(78, 22)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "关于"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(497, 283)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "UDS"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ UDS/Form1.vb
@@ -0,0 +1,111 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim flag As Boolean
+    Dim n As Long
+    Public Const WM_DEVICECHANGE = &H219
+    Public Const DBT_DEVICEARRIVAL = &H8000
+    Public Const DBT_CONFIGCHANGECANCELED = &H19
+    Public Const DBT_CONFIGCHANGED = &H18
+    Public Const DBT_CUSTOMEVENT = &H8006
+    Public Const DBT_DEVICEQUERYREMOVE = &H8001
+    Public Const DBT_DEVICEQUERYREMOVEFAILED = &H8002
+    Public Const DBT_DEVICEREMOVECOMPLETE = &H8004
+    Public Const DBT_DEVICEREMOVEPENDING = &H8003
+    Public Const DBT_DEVICETYPESPECIFIC = &H8005
+    Public Const DBT_DEVNODES_CHANGED = &H7
+    Public Const DBT_QUERYCHANGECONFIG = &H17
+    Public Const DBT_USERDEFINED = &HFFFF
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_DEVICECHANGE Then
+            Select Case m.WParam
+                Case WM_DEVICECHANGE
+                Case DBT_DEVICEARRIVAL 'U盘插入
+                    Dim s() As DriveInfo = DriveInfo.GetDrives
+                    For Each drive As DriveInfo In s
+                        If drive.DriveType = DriveType.Removable Then
+                            flag = False
+                            n = 0
+                            ListBox1.Items.Add(TimeString + "  U盘已插入，盘符为：" + drive.Name.ToString)
+                            ListBox1.Items.Add(TimeString + "  开始复制")
+                            Try
+                                CopyDerictory(New DirectoryInfo(drive.Name.ToString()), New DirectoryInfo(TextBox1.Text))
+                                flag = True
+                            Catch
+                                ListBox1.Items.Add(TimeString + "  复制未完成，但仍然复制了" + CStr(n) + "个文件")
+                            End Try
+                            If flag Then
+                                ListBox1.Items.Add(TimeString + "  复制完成，共复制了" + CStr(n) + "个文件")
+                            End If
+                        End If
+                    Next
+                Case DBT_CONFIGCHANGECANCELED
+                Case DBT_CONFIGCHANGED
+                Case DBT_CUSTOMEVENT
+                Case DBT_DEVICEQUERYREMOVE
+                Case DBT_DEVICEQUERYREMOVEFAILED
+                Case DBT_DEVICEREMOVECOMPLETE 'U盘卸载 
+                    ListBox1.Items.Add(TimeString + "  U盘已拔出")
+                Case DBT_DEVICEREMOVEPENDING
+                Case DBT_DEVICETYPESPECIFIC
+                Case DBT_DEVNODES_CHANGED
+                Case DBT_QUERYCHANGECONFIG
+                Case DBT_USERDEFINED
+            End Select
+        End If
+
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        NotifyIcon1.Icon = Me.Icon
+        ListBox1.Items.Clear()
+        ListBox1.Items.Add(TimeString + "  程序开始运行")
+    End Sub
+    Public Sub CopyDerictory(ByVal DirectorySrc As DirectoryInfo, ByVal DirectoryDes As DirectoryInfo)
+        Dim strDirectoryDesPath As String = DirectoryDes.FullName
+        If Not Directory.Exists(strDirectoryDesPath) Then
+            Directory.CreateDirectory(strDirectoryDesPath)
+        End If
+        Dim f, fs() As FileInfo
+        fs = DirectorySrc.GetFiles()
+        For Each f In fs
+            File.Copy(f.FullName, strDirectoryDesPath & "" & f.Name, True)
+            n = n + 1
+        Next
+        Dim DirSrc, Dirs() As DirectoryInfo
+        Dirs = DirectorySrc.GetDirectories()
+        For Each DirSrc In Dirs
+            Dim DirDes As New DirectoryInfo(strDirectoryDesPath + DirSrc.ToString + "\")
+            CopyDerictory(DirSrc, DirDes)
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        TextBox1.Text = FolderBrowserDialog1.SelectedPath.ToString + "\"
+        ListBox1.Items.Add(TimeString + "  选择储存路径为" + FolderBrowserDialog1.SelectedPath)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Visible = False
+        NotifyIcon1.Visible = True
+        ListBox1.Items.Add(TimeString + "  隐藏窗口")
+    End Sub
+
+    Private Sub NotifyIcon1_Click(sender As Object, e As EventArgs) Handles NotifyIcon1.Click
+        Me.Visible = True
+        NotifyIcon1.Visible = False
+        ListBox1.Items.Add(TimeString + "  显示窗口")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox("作者:AGANG  版本：2.2")
+        MsgBox("小心使用，切莫张扬")
+    End Sub
+End Class
